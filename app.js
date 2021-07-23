622c3acf2a79fc5f7ea942f4120ef84050258240
Michael Glenn <michael@valentinecreative.co>
2021-07-22 17:57:21 -0700

    App successfully relays user recent tracks route

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+
 app.use(cors());
 
 app.get("/", (req, res) => res.send("Yes, I'm here."));
+
+app.get("/userrecent", async (req, res) => {
+    try {
+        const userName = `${req.query.userName}`;
+        const limit = `${req.query.limit}`;
+        const reqUrl = `http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${userName}&limit=${limit}&api_key=${process.env.LASTFM_KEY}&format=json`;
+
+        const response = await fetch(reqUrl);
+        const json = await response.json();
+
+        return res.json({
+            success: true,
+            json,
+        });
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 app.listen(port, () => console.log(`Last.FM relay server listening on port ${port}.`));
